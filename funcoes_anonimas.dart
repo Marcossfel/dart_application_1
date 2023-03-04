5cbc3dd6d274a33bf788379f3d878147376dd4a0
Marcos Felipe da Silva Ribeiro <92560161+Marcossfel@users.noreply.github.com>
2023-03-03 22:25:57 -0300

    Functions Parameters, Anonymous Functions e Arrow Functions

---
+++ funcoes_anonimas.dart
@@ -0,0 +1,15 @@
+import 'package:dart_application_1/parametros_tipo_funcao.dart' as parametros_tipo_funcao;
+import 'package:dart_application_1/funcoes_anonimas.dart' as funcoes_anonimas;
+import 'package:dart_application_1/funcoes.dart' as funcoes;
+import 'package:dart_application_1/arrow_function.dart' as arrow_function;
+import 'package:dart_application_1/parametros_nomeados.dart' as x;
+import 'package:dart_application_1/parametros_opcionais.dart' as parametros_opcionais;
+void main(List<String> arguments) {
+  
+  /*x.sum(num1: 1, num2: 2);
+  print(x.sum(num1: 1, num2: 2));*/
+
+  parametros_opcionais.criarBotao('Marcos');
+
+
+}
